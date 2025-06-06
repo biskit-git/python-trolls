6ac81bae8417eebe9157b9acd37abfd4fcf17812
biskit-git <levgoolie@gmail.com>
2025-06-07 07:13:25 +1000

    Add files via upload

---
+++ Python.py
@@ -0,0 +1,18 @@
+﻿import pyautogui
+import subprocess
+import time
+
+def mouse_msedge():
+ while True:
+  time.sleep(0.001)
+  pyautogui.mouseDown(520, 450, duration=5)
+
+while True:
+        time.sleep(0.001)
+        subprocess.Popen("C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe")
+
+mouse_msedge()
+
+
+     
+
